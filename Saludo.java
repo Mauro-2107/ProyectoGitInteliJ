1981836c1437b57a82b433eb7b7767db0e9e028c
Mauro-2107 <maullopic@alu.edu.gva.es>
2026-01-15 13:09:42 +0100

    Creado codigo Saludo.java

---
+++ Saludo.java
@@ -0,0 +1,7 @@
+public class Saludo{
+  public void saludoInicial(){
+    System.out.println("Hola, cuanto me alegro de verte");
+  }
+  public void despedida(){
+    System.out.println("Adios, que te vaya bien");
+  }
