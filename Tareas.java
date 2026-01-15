6e98bd3dc420f33ef2ad4842c370b10dd69545a8
mllorensp <maullopic@alu.edu.gva.es>
2026-01-15 12:52:27 +0100

    Tareas.java creada

---
+++ Tareas.java
@@ -0,0 +1,8 @@
+public class Tareas {
+    public void agregarTarea(String tarea){
+        System.out.println("Tarea guardada: " + tarea);
+    }
+    public  void imprimirTarea(){
+        System.out.println("Lista: 1.Estudiar, 2.Ejercicio");
+    }
+}
